ef536f8cedf3a8a5c0ab0e2a3cc772d0fbb8aa25
Greg Burd <gburd@humio.com>
2019-12-03 15:55:32 -0500

    Evolution.

--- hosts/_common/default.nix
+++ hosts/_common/default.nix
@@ -3,26 +3,118 @@
   imports = [];
 
   boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
-  virtualisation.libvirtd.enable = true;
 
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
+  virtualisation =
+    {
+
+      # KVM
+      libvirtd = {
+        enabled = true;
+      };
+
+      # Docker
+      docker = {
+        enable = true;
+        storageDriver = "devicemapper";
+      };
+
+    };
+
+  i18n =
+    {
+      consoleFont = "Lat2-Terminus16";
+      consoleKeyMap = "us";
+      defaultLocale = "en_US.UTF-8";
+    };
 
   time.timeZone = "America/New_York";
 
   programs.mtr.enable = true;
+
+  environment.variables.EDITOR = pkgs.lib.mkOverride 0 "vim";
   programs.bash.enableCompletion = true;
 
-  environment.systemPackages = with pkgs; [
-    curl git vim
-  ];
+  environment.interactiveShellInit = ''
+    export PATH="$PATH:$HOME/bin"
+    export PAGER='less --quit-if-one-screen --no-init --ignore-case --RAW-CONTROL-CHARS --quiet --dumb';
+    parse_git_branch() {
+        git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/on \1/'
+    }
+    echo_tmux_title() {
+        printf "\033k`whoami`@`pwd`:\033\\"
+    }
+    PS1="$PS1\[\e[0;33;49m\]\$(parse_git_branch)\[\e[0;0m\]\$(echo_tmux_title)\n$ "
+    # append history instead of overwrite
+    shopt -s histappend
+    # big history, record everything
+    export HISTCONTROL=ignorespace:ignoredups:ignoreboth:erasedups
+    export HISTSIZE=300000
+    export HISTFILESIZE=200000
+    export HISTIGNORE="ls:ll:la:pwd:clear:h:j"
+    export HISTTIMEFORMAT="${FG_BLUE}${FONT_BOLD}%Y/%m/%d %H:%M:%S${FONT_RESET} "
+    export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+    # check the window size after each command and, if necessary,
+    # update the values of LINES and COLUMNS.
+    shopt -s checkwinsize
+    #tmux attach || tmux new
+  '';
 
-  services.openssh = {
-    enable = true;
-    permitRootLogin = "yes";
-  };
+
+  # Define user accounts
+  users.extraUsers =
+    {
+      gburd =
+        {
+          description = "Gregory Burd";
+          name = "gburd";
+          group = "gburd";
+          uid = 1001;
+          createHome = true;
+          extraGroups = [ "wheel" "networkmanager" "disk" "docker" "systemd-journal" ];
+          home = "/home/gburd";
+          shell = "/run/current-system/sw/bin/bash";
+          isNormalUser = true;
+          initialPassword = "password";
+        };
+    };
+
+  # Define groups for user accounts
+  users.extraGroups =
+    {
+      gburd = {
+        gid = 1001;
+      };
+    };
+
+  # Install some packages
+  environment.systemPackages =
+    with pkgs;
+    [
+      curl
+      git
+      htop
+      iotop
+      ncdu
+      nix-review
+      silver-searcher
+      tmux
+      vim
+    ];
+
+  services.openssh =
+    {
+      enable = true;
+      permitRootLogin = "yes";
+    };
+
+    system.activationScripts.dockerGc = ''
+    echo ".*-data(_[0-9]+)?" > /etc/docker-gc-exclude-containers
+    echo -e "alpine:.*\ncardforcoin/bitgo-express:.*\nclojure:.*\nmemcached:.*\nnginx:.*\npostgres:.*\npython:.*\nredis:.*\nspotify/docker-gc:.*\ntianon/true\nubuntu:.*" > /etc/docker-gc-exclude
+  '';
+
+  # Yubikey
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0402|0403|0406|0407|0410", TAG+="uaccess"
+  '';
 
 }
