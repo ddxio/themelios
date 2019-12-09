fd794539d0138a410c19db3249655f97ec959ed8
Greg Burd <gburd@humio.com>
2019-12-09 14:46:29 -0500

    WIP

--- hosts/carbon/default.nix
+++ hosts/carbon/default.nix
@@ -6,25 +6,4 @@
   networking.hostName = "carbon";
   #  environment.variables.NIXOS_CONFIG = "/nix-config/hosts/hydrogen/default.nix";
 
-  bridges =
-    {
-	    cbr0.interfaces = [ ];
-    };
-
-  interface =
-    {
-	    cbr0 = {
-		    ipAddress = "10.10.0.1";
-		    prefixLength = 24;
-	    };
-    };
-
-  services.kubernetes =
-    {
-      roles = ["master" "node"];
-    };
-
-  virtualisation.docker.extraOptions =
-    ''--iptables=false --ip-masq=false -b cbr0'';
-
 }
